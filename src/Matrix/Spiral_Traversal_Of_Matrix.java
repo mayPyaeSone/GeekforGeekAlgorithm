67551611cab129ff16936f4b4f802550dd99edde
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2025-01-24 15:22:31 +0630

    first commit.

--- src/Matrix/Spiral_Traversal_Of_Matrix.java
+++ src/Matrix/Spiral_Traversal_Of_Matrix.java
@@ -48,14 +48,48 @@ public class Spiral_Traversal_Of_Matrix {
         }
     }
 
-    public static void main(String args[]) {
-//        int arr[][] = {{1, 2, 3, 4},
-//        {5, 6, 7, 8},
-//        {9, 10, 11, 12},
-//        {13, 14, 15, 16}};
-        int arr[][] = {{1, 2, 3, 4, 5}};
+    public static ArrayList<Integer> spirallyTraverse(int mat[][]) {
+        int left = 0;
+        int right = mat[0].length - 1;
+        int top = 0;
+        int bottom = mat.length - 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                list.add(mat[top][i]);
+            }
+            top++;
+            
+            for (int i = top; i <= bottom; i++) {
+                list.add(mat[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    list.add(mat[bottom][i]);
+                }
+                bottom--;
+            }
 
-        printSpiral(arr, 1, 5);
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    list.add(mat[i][left]);
+                }
+                left++;
+            }
+
+        }
+        return list;
+    }
+
+    public static void main(String args[]) {
+        int arr[][] = {{1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}};
+       // int arr[][] = {{1, 2, 3, 4, 5}};
+
+        spirallyTraverse(arr);
 
     }
 }
