6e0ea52d36d630fade04fa0bf994f1e444ede204
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2025-02-10 14:16:53 +0630

    commit.

--- src/com/sorting/built_in/methods/Collection_With_CustomObject.java
+++ src/com/sorting/built_in/methods/Collection_With_CustomObject.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sorting.builin.methods;
+package com.sorting.built_in.methods;
 
 import java.util.*;
 import java.lang.*;
--- src/com/sorting/built_in/methods/array/sorts/SortExample.java
+++ src/com/sorting/built_in/methods/array/sorts/SortExample.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sorting.builin.methods.array.sorts;
+package com.sorting.built_in.methods.array.sorts;
 
 import java.util.Arrays;
 import java.util.Collections;
